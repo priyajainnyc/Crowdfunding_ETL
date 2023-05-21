0bbd7bea60c987dbead1ce6536c617a65df91f51
Priya Jain <priyajain@yahoo.com>
2023-05-21 07:01:23 -0400

    Project 2 Crowdfunding ETL Final Files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Crowdfunding_ETL
 Project 2 for the ETL pipeline module of Datavis bootcamp
+
+Title: The Art of Building an ETL Pipeline
+
+Summary: Build an ETL pipeline using Python, Pandas and either Python dictionary methods or regular expressions to extract and transform the data. After you transform the data, create four CSV files and use the CSV file data to create an ERD and a table schema, and finally, upload the CSV file data into a Postgres database.
+
+Guide Below: Imports, Resources, Files and PNGs 
+
+Packages Imported: pandas as pd, numpy as np, pd.set_option('max_colwidth', 400), from datetime import datetime as dt, import json, import csv as dataframe 
+
+Resources Used: Files called "crowdfunding.xlsx" and "contacts.xlsx" which contain the raw data
+
+Notebooks Created: 
+
+Python Notebook File With Full Code For All CSV Files Called: ETL_Mini_Project_Starter_Code.ipynb
+
+CSV Files Produced From Notebooks and Subsequently Imported Into Postgres Located In "Resources" Folder Called: "campaign.csv", "category.csv", "contacts.csv", "subcategory.csv"
+
+SQL File Exported from QuickDBD And Run In Postgres Called: "crowdfinding_db_schema.sql"
+
+PNGs Created From: 
+1. Screenshot of Crowdfunding Database ERD From QuickDBD Called: "crowdfunding_db_ERD.png"
+2. Screenshots Of Tables Created In Postgres By Running CSV File Imports Using Schema Called: "campaign.png", "category.png", "contacts.png", "subcategory.png"
---
+++ crowdfinding_db_schema.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/FHHOil
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "contacts" (
+    "contact_id" INT(5)   NOT NULL,
+    "first_name" VARCHAR(25)   NOT NULL,
+    "last_name" VARCHAR(25)   NOT NULL,
+    "email" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" INT(5)   NOT NULL,
+    "contact_id" INT(5)   NOT NULL,
+    "company_name" VARCHAR(25)   NOT NULL,
+    "description" VARCHAR(50)   NOT NULL,
+    "goal" FLOAT(10)   NOT NULL,
+    "pledged" FLOAT(10)   NOT NULL,
+    "outcome" VARCHAR(15)   NOT NULL,
+    "backers_count" INT(5)   NOT NULL,
+    "country" VARCHAR(5)   NOT NULL,
+    "currency" VARCHAR(5)   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "categpry_id" VARCHAR(5)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(5)   NOT NULL,
+    "category" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    "subcategory" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_categpry_id" FOREIGN KEY("categpry_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
